0c4b45632545a109efb1750b0688983e7ede60a3
nachtgastWork <mailforjs@gmail.com>
2022-07-04 10:49:03 +0300

    TicTacToe update finally

--- src/src/com/hiperskill/tictactoe/Main.java
+++ src/src/com/hiperskill/tictactoe/Main.java
@@ -1,51 +1,132 @@
 package com.hiperskill.tictactoe;
 
+
 import java.util.Scanner;
 
 public class Main {
-    public static void printField(char[][] field) {
+    final static char[][] array = new char[3][3];
+
+    public static void main(String[] args) {
+        initArray();
+        printGrid();
+        while (true) {
+            enterCoordinates('X');
+            printGrid();
+            if (checkGrid() == 1)
+                break;
+            enterCoordinates('O');
+            printGrid();
+            if (checkGrid() == 1)
+                break;
+        }
+    }
+
+    static void initArray() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = ' ';
+            }
+        }
+    }
+
+    static void printGrid() {
         System.out.println("---------");
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < 3; i++) {
             System.out.print("| ");
-            for (int j = 0; j < 3; ++j) {
-                System.out.print(field[i][j] + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(array[i][j] + " ");
             }
             System.out.println("|");
         }
         System.out.println("---------");
     }
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        String x = s.next();
-        char[][] field = new char[3][3];
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                field[i][j] = x.charAt(i * 3 + j);
-            }
-        }
-        printField(field);
+    static void enterCoordinates(char ch) {
+        Scanner     scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter the coordinates: ");
-            int x1, x2;
+            System.out.print("Enter the coordinates: ");
+            String str = scanner.nextLine();
+            String[] coordinates = str.split(" ");
+
+            if (coordinates[0].equals("exit")) {
+                System.exit(0);
+            } else if (coordinates.length != 2) {
+                System.out.println("You should enter 2 numbers!");
+                continue;
+            }
             try {
-                x1 = s.nextInt();
-                x2 = s.nextInt();
-                if (x1 < 1 || x1 > 3 || x2 < 1 || x2 > 3) {
+                int x = Integer.parseInt(coordinates[0]);
+                int y = Integer.parseInt(coordinates[1]);
+
+                if (x < 1 || x > 3 || y < 1 || y > 3) {
                     System.out.println("Coordinates should be from 1 to 3!");
+                } else if (array[x - 1][y - 1] != ' ') {
+                    System.out.println("This cell is occupied! Choose another one!");
                 } else {
-                    if (field[x1 - 1][x2 - 1] != '_') {
-                        System.out.println("This cell is occupied! Choose another one!");
-                    } else {
-                        field[x1 - 1][x2 - 1] = 'X';
-                        break;
-                    }
+                    array[x - 1][y - 1] = ch;
+                    break;
                 }
-            } catch (Exception e) {
+            } catch (NumberFormatException e) {
                 System.out.println("You should enter numbers!");
             }
         }
-        printField(field);
+    }
+
+    static int checkGrid() {
+        if (checkHorizonAndVertical() == 1) {
+            return 1;
+        } else if (array[0][0] != ' ' && array[0][0] == array[1][1] && array[1][1] == array[2][2]) {
+            System.out.println(array[0][0] + " wins");
+            return 1;
+        } else if (array[0][2] != ' ' && array[0][2] == array[1][1] && array[1][1] == array[2][0]) {
+            System.out.println(array[0][2] + " wins");
+            return 1;
+        } else {
+            return countCharacters();
+        }
+    }
+
+    static int  checkHorizonAndVertical() {
+        for (int i = 0; i < 3; i++) {
+            if (array[i][0] != ' ' && array[i][0] == array[i][1] && array[i][1] == array[i][2]) {
+                System.out.println(array[i][0] + " wins");
+                return 1;
+            }
+            if (array[0][i] != ' ' && array[0][i] == array[1][i] && array[1][i] == array[2][i]) {
+                System.out.println(array[0][i] + " wins");
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    static int countCharacters() {
+        int     x = 0;
+        int     o = 0;
+        int     space = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (array[i][j] == 'X') {
+                    x++;
+                } else if (array[i][j] == 'O') {
+                    o++;
+                } else {
+                    space++;
+                }
+            }
+        }
+        return checkCharacters(x, o, space);
+    }
+
+    static int  checkCharacters(int x, int o, int space) {
+        if (Math.abs(x - o) > 1) {
+            System.out.println("Impossible");
+            return 1;
+        } else if (space == 0) {
+            System.out.println("Draw");
+            return 1;
+        }
+        return 0;
     }
 }
